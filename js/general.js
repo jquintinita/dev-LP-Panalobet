db2af74a4cd7ee53d827c53fba966aafb21ee788
Joseph Quintinita <joseph_quintinita@worldvision.ca>
2024-02-07 08:45:22 +0800

    added assets and source code

---
+++ js/general.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // Swiper: Slider
+        new Swiper('.hero-slider', {
+            loop: true,
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 1,
+            paginationClickable: true,
+            spaceBetween: 0,
+            
+        });
+    });
+    
