cd4bb2b2b712a5f90dc4d2e723e7c582e37ee20d
Joseph Quintinita <joseph_quintinita@worldvision.ca>
2024-02-08 11:09:12 +0800

    update 02082024

--- js/general.js
+++ js/general.js
@@ -1,13 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Swiper: Slider
-        new Swiper('.hero-slider', {
-            loop: true,
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-            slidesPerView: 1,
-            paginationClickable: true,
-            spaceBetween: 0,
-            
-        });
+    new Swiper('.promo-slider', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        effect: 'coverflow',
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 1,
+        spaceBetween: -100,
+        coverflow: {
+            rotate: 0,
+            stretch: 0,
+            depth: 800,
+            modifier: 1,
+            slideShadows: false,
+        }
+
     });
-    
+
+    new Swiper('.hero-slider', {
+        loop: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        slidesPerView: 1,
+        paginationClickable: true,
+        spaceBetween: 0,
+
+    });
+
+
+
+    //SMOOTH SCROLL
+
+
+
+});
